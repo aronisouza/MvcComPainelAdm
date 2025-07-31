96343e9ebc4037d3194de832f457f6673ffbe5ce
aronisouza <aronisouza@yahoo.com.br>
2025-07-31 19:32:12 -0300

    Editado redirecionamento da checklogin para home do Site

--- Core/Controller.php
+++ Core/Controller.php
@@ -1,8 +1,10 @@
 <?php
 
-class Controller {
+class Controller
+{
 
-    protected function render($view, $data = []) {
+    protected function render($view, $data = [])
+    {
         // Extrai os dados para variáveis no escopo local
         extract($data);
 
@@ -14,19 +16,20 @@ class Controller {
             // Define a variável $content com o caminho da view
             //-- Usada na template.php para carregar a view
             $content = $viewPath;
-             // Carrega o template
-             if (strpos($content, 'Controlador') !== false && (isset($_SESSION['email']) && !empty($_SESSION['email']))) require_once __DIR__ . "/../Views/Controlador/base.php";
-             else require_once __DIR__ . "/../Views/base.php";
+            // Carrega o template
+            if (strpos($content, 'Controlador') !== false && (isset($_SESSION['email']) && !empty($_SESSION['email']))) require_once __DIR__ . "/../Views/Controlador/base.php";
+            else require_once __DIR__ . "/../Views/base.php";
         } else {
             // Se a view não existir, carrega a página de erro 404
             require_once __DIR__ . "/../Views/errors/404.php";
         }
     }
 
-    protected function validateCsrfToken($token) {
+    protected function validateCsrfToken($token)
+    {
         return isset($_SESSION['csrf_token']) && hash_equals($_SESSION['csrf_token'], $token);
     }
-    
+
     /**
      * $icone => success, error, warning, info, question
      * 
@@ -40,12 +43,13 @@ class Controller {
      * 
      *
      */
-    protected static function setErrorAndRedirect($mensagem, $redirectUrl, $titulo = 'Erro', $icone = 'error') {
+    protected static function setErrorAndRedirect($mensagem, $redirectUrl, $titulo = 'Erro', $icone = 'error')
+    {
         setErrorMessage($mensagem, $titulo, $icone);
         header("Location: {$redirectUrl}");
         exit;
     }
-    
+
     /**
      * 
      * Define uma mensagem de sucesso e redireciona para a URL fornecida
@@ -55,19 +59,22 @@ class Controller {
      * @param string $titulo Título da mensagem (opcional)
      * @return void
      */
-    protected function setSuccessAndRedirect($mensagem, $redirectUrl, $titulo = 'Sucesso') {
+    protected function setSuccessAndRedirect($mensagem, $redirectUrl, $titulo = 'Sucesso')
+    {
         setSuccessMessage($mensagem, $titulo);
         header("Location: {$redirectUrl}");
         exit;
     }
 
-    
     protected function checklogin()
     {
         if (!isset($_SESSION['role']) && $_SESSION['role'] != 'admin') {
-            $this->render('Controlador/Login');
-            return;
+
+            $this->setErrorAndRedirect(
+                "Requisição inválida. Visitante enviado para Home do Site.",
+                "/",
+                "Erro de Segurança"
+            );
         }
     }
-    
 }
